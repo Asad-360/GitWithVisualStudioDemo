9fc6b496733e88a8133c8b32b3cc130a92cab1da
Asad Mukhtar <43516964+Asad-Mukhtar@users.noreply.github.com>
2020-02-08 09:06:26 -0800

    AddedMethodForPrintingEvenNumbers

--- VisualStudioGit/Program.cs
+++ VisualStudioGit/Program.cs
@@ -20,5 +20,14 @@ namespace VisualStudioGit
                 Console.WriteLine(i);
             }
         }
+        static void PrintEventNumbers()
+        {
+            for(int i=0 ; i < 20 ; i++)
+            {
+                if(i % 2 == 0)
+                    Console.WriteLine(i);
+            }
+            
+        }
     }
 }
